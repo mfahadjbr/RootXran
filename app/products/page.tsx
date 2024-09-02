b5f327c6c510304e5ca01aaf4c6efd0b0578dd93
mfahadjbr <mfahadjbr@gmail.com>
2024-09-02 10:32:40 +0500

    Update page.tsx

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -7,9 +7,9 @@ import Link from "next/link";
 
 export default function ThreeDCardDemo() {
   return (
-    <div className="flex flex-wrap justify-center gap-10">
+    <div className="flex flex-wrap justify-center gap-10 mb-[10px]">
     <CardContainer className="inter-var shadow-2xl rounded-2xl">
-      <CardBody className="bg-gray-50 relative group/card mb-[10px] dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
+      <CardBody className="bg-gray-50 relative group/card dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
         <CardItem
           translateZ="50"
           className="text-2xl font-bold text-neutral-600 dark:text-white"
